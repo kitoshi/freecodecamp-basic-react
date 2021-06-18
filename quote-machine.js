78c9c8a256a64d994e6006e38fea83f03801d1de
kitoshi <kitoshi.charlton@gmail.com>
2021-06-17 23:58:38 -0700

    final basic arrays

---
+++ quote-machine.js
@@ -0,0 +1,21 @@
+class QuoteMachine extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        quote: ''
+      };
+      handleChange(event) {
+        this.setState({ input: event.target.value})
+      }
+       render() {
+         return (
+            <div>
+            <h1>Popular Front End JavaScript Frameworks</h1>
+            <ul>
+              {renderFrameworks}
+            </ul>
+          </div>
+         )
+       }
+    }
+  }
